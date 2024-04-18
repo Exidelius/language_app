958d3148f4a8b4e34d44bad1c6206d5a081015c1
Mikhail <mir10102@mail.ru>
2024-04-18 08:08:33 +0300

    Смена темы. Наверное. По кнопке переключает нижнюю понель, а при смене системной темы переключается тема приложения

--- app/src/main/java/com/screens/language_app/Profile.java
+++ app/src/main/java/com/screens/language_app/Profile.java
@@ -1,13 +1,16 @@
 package com.screens.language_app;
 
+import android.app.UiModeManager;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.activity.ComponentActivity;
 import androidx.annotation.Nullable;
-import androidx.compose.ui.semantics.ProgressBarRangeInfo;
 
+import com.example.mobileapp.MainActivity;
 import com.example.mobileapp.R;
 
 public class Profile extends ComponentActivity {
@@ -18,7 +21,19 @@ public class Profile extends ComponentActivity {
     }
 
     public void switch_theme(View v) {
-        startActivity(new Intent(Profile.this, Profile.class));
+        Configuration configuration = getResources().getConfiguration();
+        int currentNightMode = configuration.uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                Log.d("Тема", "Светлая тема");
+                configuration.uiMode = Configuration.UI_MODE_NIGHT_YES;
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                Log.d("Тема", "Тёмная тема");
+                configuration.uiMode = Configuration.UI_MODE_NIGHT_NO;
+                break;
+        }
+        recreate();
     }
 
     public void change_language(View v) {
